245b907056b45e6716baeffdb1067cb66eda243d
Nacho Miranda <nachomirnd@gmail.com>
2026-09-17 19:59:03 -0300

    fix: allow all origins dynamically in CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:8080'] : '*',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
